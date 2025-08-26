81cb53462b6016e9196ad315fbfccb00c93fe78a
SeeTwoo <walti.bee@gmail.com>
2025-08-26 15:07:54 +0200

    fortschritt fortschritt

--- C01/ex00/src/Zombie.cpp
+++ C01/ex00/src/Zombie.cpp
@@ -4,3 +4,19 @@ void	Zombie::announce(void) {
 	std::cout << name << ": BraiiiiiiinnnzzzZ...\n";`
 }
 
+Zombie	*newZombie(std::string "name") {
+	Zombie	*dest;
+
+	dest = new();
+	if (!dest)
+		return (NULL);
+	dest.name = name;
+	return (dest);
+}
+
+void	Zombie::randomChump(std::string name) {
+	Zombie	zombie;
+
+	zombie.name = "Karl";
+	zombie.announce();
+}
