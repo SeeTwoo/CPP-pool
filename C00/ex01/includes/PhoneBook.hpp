3bfce5946660c4722944ad27a9df9490632a6273
Walter Beschon <wbeschon@f6r8s5.clusters.42paris.fr>
2025-08-30 19:06:51 +0200

    hello

--- C00/ex01/includes/PhoneBook.hpp
+++ C00/ex01/includes/PhoneBook.hpp
@@ -6,15 +6,11 @@
 
 class PhoneBook {
 	public:
-//		PhoneBook();
-//		~PhoneBook();
-//		PhoneBook(const PhoneBook&);
-//		PhoneBook& operator=(const PhoneBook&);
-
 		void	addContact(void);
 		void	searchContact(void);
 	private:
 		Contact	contacts[9];
+		int		currentIndex = 0;
 };
 
 #endif
--- C00/ex01/src/PhoneBook.cpp
+++ C00/ex01/src/PhoneBook.cpp
@@ -1,9 +1,45 @@
 #include "PhoneBook.hpp"
 
-void	PhoneBook::addContact(void) {
+PhoneBook::addContact() {
 	std::cout << "adding a contact\n";
 }
 
-void	PhoneBook::searchContact(void) {
-	std::cout << "searching for a contact\n";
+
+
+PhoneBook::displayPreview() {
+	for (int i = 0; i++; i <= this->currentIndex) {
+		printFieldPreview(this->contacts[i]->_firstName;);
+		std::cout << "|";
+		printFieldPreview(this->contacts[i]->_lastName;);
+		std::cout << "|";
+		printFieldPreview(this->contacts[i]->_nickName;);
+		std::cout << "|";
+		printFieldPreview(this->contacts[i]->_phoneNumber;);
+		std::cout << "|";
+		printFieldPreview(this->contacts[i]->_secret;);
+		std::cout << "|";
+	}
+}
+
+PhoneBook::searchContact() {
+	std::string	indexString;
+	int			contactIndex;
+
+	this->displayPreview();
+	std::cout << "Enter contact index > ";
+	indexString = std::getline(std::cin, std::indexString);
+	if (!index) {
+		std::cerr << "getline failed \n";
+		return ;
+	}
+	if (!std::isdigit(indexString[0])) {
+		std::cerr << "The index must only contain digits\n";
+		return ;
+	}
+	contactIndex = std::stoi(indexString);
+	if (contactIndex < 0 || contactIndex > 7) {
+		std::cerr << "The index must be in the 0 to 7 range\n";
+		return ;
+	}
+	this->displayContact(contactIndex);
 }
