990cb08ccfae3835a2d8e5f3f50614aee8fb62ef
Walter Beschon <wbeschon@f6r7s12.clusters.42paris.fr>
2025-09-06 17:53:23 +0200

    added conditional space in megaphone

--- C00/ex00/src/megaphone.cpp
+++ C00/ex00/src/megaphone.cpp
@@ -8,7 +8,8 @@ int	main(int ac, char **av) {
 	for (int i = 1; av[i]; i++) {
 		for (int j = 0; av[i][j]; j++)
 			std::cout << (char)toupper(av[i][j]);
-		std::cout << " ";
+		if (av[i + 1])
+			std::cout << " ";
 	}
 	std::cout << std::endl;
 	return 0;
