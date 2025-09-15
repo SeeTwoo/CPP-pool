ccc8791793c29c90ce6264eca938f94ccac18077
SeeTwoo <walti.bee@gmail.com>
2025-09-15 23:45:56 +0200

    hello

--- C03/ex01/src/main.cpp
+++ C03/ex01/src/main.cpp
@@ -11,14 +11,17 @@
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int	main() {
 	ClapTrap	daniel("daniel");
 	ClapTrap	manu("manu");
+	ScavTrap	karl("karl");
 
 	daniel.attack("manu");
 	manu.attack("daniel");
 	manu.takeDamage(10);
 	manu.attack("daniel");
+	karl.attack("daniel");
 	return 0;
 }
