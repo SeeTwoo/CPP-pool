7d12e978638901142b894376f6a38f2c0bcc6147
SeeTwoo <walti.bee@gmail.com>
2025-09-16 18:54:42 +0200

    hello

---
+++ C04/ex00/src/Animal.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Animal.hpp"
+
+void	makeSound() {
+
+}
+
+Animal::Animal() {
+	std::cout << "Animal default constructor called\n";
+}
+
+Animal::Animal(const Animal &other) : type(other.type) {
+	std::cout << "Animal copy constructor called\n";
+}
+
+Animal	&operator=(const Animal &other) {
+	if (this == &other)
+		return *this;
+	type = other.type;
+	std::cout << "Animal equal operator called\n";
+	return *this;
+}
+
+Animal::~Animal() {
+	std::cout << "Animal default destructor called\n";
+}
