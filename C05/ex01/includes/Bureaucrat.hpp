b642543a79c2e3c42d413b57351bcda0a52814c8
SeeTwoo <walti.bee@gmail.com>
2025-10-25 14:20:54 +0200

    hello

---
+++ C05/ex01/includes/Bureaucrat.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         :::     ::::::::   */
+/*   Bureaucrat.hpp                                      :+:     :+:    :+:   */
+/*                                                     +:+ +:+        +:+     */
+/*   By: walter </var/spool/mail/walter>             +#+  +:+       +#+       */
+/*                                                 +#+#+#+#+#+   +#+          */
+/*   Created:                                           #+#    #+#            */
+/*   Uptated:                                          ###   ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <string>
+
+class Bureaucrat {
+	private :
+		std::string const	_name;
+		int					_grade;
+	public :
+		class GradeTooHighException : public std::exception {
+			public :
+				const char* what() const throw();
+		};
+		class GradeTooLowException : public std::exception {
+			public :
+				const char* what() const throw();
+		};
+		const std::string	getName() const ;
+		int					getGrade() const ;
+
+		void		incrementGrade();
+		void		decrementGrade();
+
+		Bureaucrat();
+		Bureaucrat(int, const std::string);
+		Bureaucrat(const Bureaucrat &);
+		Bureaucrat&	operator=(const Bureaucrat &);
+		~Bureaucrat();
+};
+
+std::ostream& operator<<(std::ostream &, const Bureaucrat &);
+
+#endif
---
+++ C05/ex01/src/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         :::     ::::::::   */
+/*   main.c                                              :+:     :+:    :+:   */
+/*                                                     +:+ +:+        +:+     */
+/*   By: seetwoo <marvin@42students.fr>              +#+  +:+       +#+       */
+/*                                                 +#+#+#+#+#+   +#+          */
+/*   Created:                                           #+#    #+#            */
+/*   Uptated:                                          ###   ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "Bureaucrat.hpp"
+
+int	main() {
+	{
+		try {
+			Bureaucrat	dave(200, "dave");
+			std::cout << dave;
+		} catch (std::exception &e) {
+			std::cout << e.what();
+		}
+	}
+	{
+		try {
+			Bureaucrat	sam(148, "sam");
+			std::cout << sam;
+			sam.decrementGrade();
+			std::cout << sam;
+			sam.decrementGrade();
+			std::cout << sam;
+			sam.decrementGrade();
+			std::cout << sam;
+		} catch (std::exception &e) {
+			std::cout << e.what();
+		}
+	}
+	return 0;
+}
