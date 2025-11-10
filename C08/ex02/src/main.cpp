2b0662fbd7a3ce73a129ea0a2790492b0722bb73
Walter Beschon <wbeschon@f6r8s2.clusters.42paris.fr>
2025-11-10 19:54:31 +0100

    hello

---
+++ C08/ex02/src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "MutantStack.hpp"
+
+int main(void) {
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+	// Reverse iteration example:
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+
+	while (rit != rite) {
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+
+	return 0;
+}
