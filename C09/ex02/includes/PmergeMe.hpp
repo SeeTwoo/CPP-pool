c9d3771643a6fc3a997cd97f9c9a8de7167e0aa2
Walter Beschon <wbeschon@f6r7s12.clusters.42paris.fr>
2025-12-19 15:49:23 +0100

    replaced ex02 for good

--- C09/ex02/includes/PmergeMe.hpp
+++ C09/ex02/includes/PmergeMe.hpp
@@ -1,38 +1,28 @@
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
-# include <iostream>
-# include <string>
 # include <vector>
 # include <deque>
-# include <algorithm>
-# include <ctime>
-# include <iomanip>
-# include <climits>
-# include <cstdlib>
+
+std::vector<std::size_t>	computeJacobsthal(std::size_t n);
+std::vector<int>			makePending(const std::vector<std::pair<int, int> >& pairs);
 
 class PmergeMe {
-    private:
-        std::vector<int>    _vec;
-        std::deque<int>     _deq;
-        double              _timeVec;
-        double              _timeDeq;
+	private :
+		PmergeMe();
+		PmergeMe(const PmergeMe &);
+		PmergeMe	&operator=(const PmergeMe &);
+		~PmergeMe();
+	public :
 
-        void    _mergeInsertSortVec(std::vector<int>& arr);
-        void    _insertInSortedVec(std::vector<int>& dest, int val);
-        
-        void    _mergeInsertSortDeq(std::deque<int>& arr);
-        void    _insertInSortedDeq(std::deque<int>& dest, int val);
-
-        std::vector<int> _generateInsertionOrder(size_t size);
-
-    public:
-        PmergeMe();
-        ~PmergeMe();
-        PmergeMe(const PmergeMe& src);
-        PmergeMe& operator=(const PmergeMe& src);
-
-        void    sortAndMeasure(int argc, char **argv);
+		template <typename Container>
+		static void	sort(Container &);
+		template <typename Iterator>
+		static bool isSorted(Iterator begin, Iterator end);
+		template <typename Sorter, typename Container>
+		static unsigned long	benchmarkSorting(Sorter sorter, Container& container);
 };
+
+# include "PmergeMe.tpp"
 
 #endif
